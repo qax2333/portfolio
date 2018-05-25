296bae28ec5c5b81b993f06fd115f76926de6310
Kenneth Wußmann <>
2018-05-26 01:54:52 +0200

    Added mail queue to reduce page loading time

--- server/src/main/java/de/ketrwu/portfolio/PortfolioApplication.java
+++ server/src/main/java/de/ketrwu/portfolio/PortfolioApplication.java
@@ -7,10 +7,12 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableAutoConfiguration
 @Configuration
+@EnableScheduling
 @ComponentScan("de.ketrwu.portfolio")
 public class PortfolioApplication {
 
--- server/src/main/java/de/ketrwu/portfolio/service/impl/MailServiceImpl.java
+++ server/src/main/java/de/ketrwu/portfolio/service/impl/MailServiceImpl.java
@@ -2,16 +2,22 @@ package de.ketrwu.portfolio.service.impl;
 
 import de.ketrwu.portfolio.forms.ContactForm;
 import de.ketrwu.portfolio.service.MailService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.mail.javamail.MimeMessagePreparator;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
 
+import java.util.ArrayList;
+import java.util.List;
 
+@Slf4j
 @Service
 public class MailServiceImpl implements MailService {
 
@@ -27,6 +33,8 @@ public class MailServiceImpl implements MailService {
     @Value("${app.mail.owner}")
     private String mailOwner;
 
+    private List<MimeMessagePreparator> mailQueue = new ArrayList<>();
+
     @Override
     public void sendMail(ContactForm contactForm) {
         String ownerMail = buildEmail(contactForm, true);
@@ -36,15 +44,14 @@ public class MailServiceImpl implements MailService {
     }
 
     private void sendMailTemplate(String to, String replyTo, String subject, String content) {
-        MimeMessagePreparator messagePreparator = mimeMessage -> {
+        mailQueue.add(mimeMessage -> {
             MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage);
             messageHelper.setFrom(mailSender);
             messageHelper.setTo(to);
             messageHelper.setReplyTo(replyTo);
             messageHelper.setSubject(subject);
             messageHelper.setText(content, true);
-        };
-        emailSender.send(messagePreparator);
+        });
     }
 
     private String buildEmail(ContactForm contactForm, boolean owner) {
@@ -54,6 +61,18 @@ public class MailServiceImpl implements MailService {
             return templateEngine.process("mails/owner-notify", context);
         } else {
             return templateEngine.process("mails/sender-notify", context);
+        }
+    }
+
+    // every 10 minutes
+    @Scheduled(fixedRate = 600000)
+    private void sendMailQueue() {
+        if (mailQueue.size() > 0) {
+            log.info("Sending {} mails from queue", mailQueue.size());
+            for (MimeMessagePreparator mimeMessagePreparator : mailQueue) {
+                emailSender.send(mimeMessagePreparator);
+            }
+            mailQueue.clear();
         }
     }
 
