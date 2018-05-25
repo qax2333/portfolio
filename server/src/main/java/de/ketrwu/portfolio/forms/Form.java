e1fc226dc96d810b94582ce4d3d9c9016e2a0967
Kenneth Wußmann <>
2018-05-26 01:11:05 +0200

    Added missing validation

--- server/src/main/java/de/ketrwu/portfolio/forms/Form.java
+++ server/src/main/java/de/ketrwu/portfolio/forms/Form.java
@@ -3,10 +3,13 @@ package de.ketrwu.portfolio.forms;
 import de.ketrwu.portfolio.forms.validation.formtoken.FormToken;
 import lombok.Data;
 
+import javax.validation.constraints.NotEmpty;
+
 @Data
 @FormToken(message = "Dieses Formular kann nicht erneut abgesendet werden.")
 public class Form {
 
+    @NotEmpty
     private String formToken;
 
 }
