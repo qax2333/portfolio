b32396079c7aa317cbf76c7266420b16b01553df
Kenneth Wußmann <kenneth@wussmann.net>
2018-11-17 12:10:01 +0100

    #16 Added project meta data and auto-generate project list on home-page

--- server/src/main/kotlin/de/ketrwu/portfolio/entity/MarkdownTextPage.kt
+++ server/src/main/kotlin/de/ketrwu/portfolio/entity/MarkdownTextPage.kt
@@ -2,33 +2,52 @@ package de.ketrwu.portfolio.entity
 
 data class MarkdownTextPage(
     var rendered: String = "",
-    var meta: MarkdownMeta = MarkdownMeta()
-)
-
-data class MarkdownMeta(
-    var filename: String = "",
-    var title: String = "",
-    var headline: String = "",
-    var description: String = "",
-    var tags: List<String> = listOf()
+    var meta: MarkdownMeta = MarkdownMeta(null)
 ) {
-    companion object {
-        fun of(filename: String, yamlMeta: Map<String, List<String>>): MarkdownMeta {
-            val title = yamlMeta["title"]!![0]
-            val headline = yamlMeta["headline"]!![0]
-            val description = yamlMeta["description"]!![0]
-            val tags = if (yamlMeta.containsKey("tags")) {
-                yamlMeta["tags"]!!.map { it.toLowerCase() }
-            } else {
-                listOf()
-            }
-            return MarkdownMeta(
-                filename,
-                title,
-                headline,
-                description,
-                tags
-            )
+    fun getPath(): String {
+        return "/page" + if (meta.tags.isEmpty()) {
+            "/${meta.filename}"
+        } else {
+            "/${meta.tags.first()}/${meta.filename}"
         }
+    }
+}
+
+open class MarkdownMeta(
+    open var filename: String?,
+    open var title: String = "",
+    open var headline: String = "",
+    open var description: String = "",
+    open var tags: List<String> = listOf()
+) {
+    open fun of(yamlMeta: Map<String, List<String>>): MarkdownMeta {
+        title = yamlMeta["title"]!![0]
+        headline = yamlMeta["headline"]!![0]
+        description = yamlMeta["description"]!![0]
+        tags = if (yamlMeta.containsKey("tags")) {
+            yamlMeta["tags"]!!.map { it.toLowerCase() }
+        } else {
+            listOf()
+        }
+        return this
+    }
+}
+
+class ProjectMarkdownMeta(
+    override var filename: String?,
+    override var title: String = "",
+    override var headline: String = "",
+    override var description: String = "",
+    override var tags: List<String> = listOf(),
+    var languages: List<String> = listOf()
+) : MarkdownMeta(filename, title, headline, description, tags) {
+    override fun of(yamlMeta: Map<String, List<String>>): ProjectMarkdownMeta {
+        super.of(yamlMeta)
+        languages = if (yamlMeta.containsKey("languages")) {
+            yamlMeta["languages"]!!
+        } else {
+            listOf()
+        }
+        return this
     }
 }
